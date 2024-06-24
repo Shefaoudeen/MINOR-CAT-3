0bef39b80dd6f8463d9317603d38ea664d76adfb
Shefa <shefaoudeen123@gmail.com>
2024-06-24 20:46:01 +0530

    add

--- crud.php
+++ crud.php
@@ -86,3 +86,20 @@
         $stmt->close();
     }
 ?>
+
+//delete
+<?php
+  $id = $_POST['id'];
+
+  $sql = "DELETE FROM users WHERE id = ?";
+
+  if ($stmt = $conn->prepare($sql)) {
+      $stmt->bind_param("i", $id);
+      if ($stmt->execute()) {
+          echo "User deleted successfully!";
+      } else {
+          echo "Error: " . $stmt->error;
+      }
+      $stmt->close();
+  }
+?>
